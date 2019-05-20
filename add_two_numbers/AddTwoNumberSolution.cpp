54d06290fbeb7009a366fe839af3ad330b5020d1
yunfeng <zong8498@gmail.com>
2019-05-20 16:23:36 +0800

    finish my addTwoNumber solution

---
+++ add_two_numbers/AddTwoNumberSolution.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Yunfeng on 2019-04-23.
+//
+
+struct ListNode {
+    int val;
+
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class AddTwoNumberSolution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *i = l1;
+        ListNode *j = l2;
+        auto *head = new ListNode(0);
+        auto *current = head;
+        auto *nextNode;
+        int carry{0};
+
+        while (i != nullptr || j != nullptr) {
+            int x = (i != nullptr) ? i->val : 0;
+            int y = (j != nullptr) ? j->val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            nextNode = new ListNode(sum % 10);
+            current->next = nextNode;
+            current = current->next;
+            if (i != nullptr) i = i->next;
+            if (j != nullptr)j = j->next;
+        }
+
+        if (carry > 0) {
+            nextNode = new ListNode(carry);
+            current->next = nextNode;
+        }
+
+        return head->next;
+    }
+};
